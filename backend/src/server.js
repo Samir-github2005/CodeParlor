487d6c2f5a5bfc4c33ec5076cd765e9693325f6d
Samir Bhakta <bhaktasamir11042005@gmail.com>
2026-03-02 01:33:35 +0530

    deployment added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from "express"
+import path from "path"
 import { ENV } from "./lib/env.js"
 const port= ENV.PORT
+const __dirname=path.resolve()
 const app=express()
-app.get('/',(req,res)=>{
+app.get('/health',(req,res)=>{
     res.send("hey")
 })
+
+if(ENV.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+    app.get('/{*any}',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'))
+    })
+}
 
 app.listen(port,()=>{
     console.log(`server running on port ${port}`)
