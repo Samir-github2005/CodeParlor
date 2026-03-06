ea78cd4626cd86668d491c2bb1587960d5503331
Samir Bhakta <bhaktasamir11042005@gmail.com>
2026-03-06 23:18:42 +0530

    added function

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import { ENV } from "./lib/env.js"
 import { connectDb } from "./lib/db.js"
 import cors from "cors"
 import {serve} from "inngest/express"
-import { inngest } from "./lib/inngest.js"
+import { inngest, functions } from "./lib/inngest.js"
 
 const port= ENV.PORT
 const __dirname=path.resolve()
@@ -12,7 +12,7 @@ const app=express()
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
-app.use("/api/inngest",serve({ client: inngest, functions:[]}))
+app.use("/api/inngest",serve({ client: inngest, functions}))
 
 app.get('/health',(req,res)=>{
     res.send("hey")
