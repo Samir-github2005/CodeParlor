8bf932978ad5ea22ddc1b052b5d6b8e86f398554
Samir Bhakta <bhaktasamir11042005@gmail.com>
2026-03-07 23:50:38 +0530

    serverSide auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,23 @@ import { connectDb } from "./lib/db.js"
 import cors from "cors"
 import {serve} from "inngest/express"
 import { inngest, functions } from "./lib/inngest.js"
+import {clerkMiddleware} from "@clerk/express"
+import chatRoutes from "./routes/chatRoutes.js"
 
 const port= ENV.PORT
 const __dirname=path.resolve()
 const app=express()
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
-
+app.use(clerkMiddleware())
 app.use("/api/inngest",serve({ client: inngest, functions}))
+
+app.use('/api/chat',chatRoutes)
 
 app.get('/health',(req,res)=>{
     res.send("hey")
 })
+
 
 if(ENV.NODE_ENV=="production"){
     app.use(express.static(path.join(__dirname,'../frontend/dist')))
