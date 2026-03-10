1bd48fd19562da8f7ce93f79f487bbd94910547f
Samir Bhakta <bhaktasamir11042005@gmail.com>
2026-03-10 18:56:59 +0530

    added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { Navigate, Route, Routes } from 'react-router'
-import {HomePage} from './pages/HomePage'
-import { ProblemPage } from './pages/ProblemPage'
+import HomePage from './pages/HomePage.jsx'
+import { ProblemPage } from './pages/ProblemPage.jsx'
 import { useUser } from '@clerk/clerk-react'
 import {Toaster} from 'react-hot-toast'
+import DashboardPage  from './pages/DashboardPage.jsx'
 
 
 function App() {
-const {isSignedIn} = useUser()
+const {isSignedIn, isLoaded} = useUser()
+
+if(!isLoaded) return null
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/problems' element={isSignedIn? <ProblemPage/> : Navigate({ to:'/'})}/>
+        <Route path='/' element={!isSignedIn? <HomePage/> : <Navigate to='/dashboard'/>}/>
+        <Route path='/dashboard' element={isSignedIn? <DashboardPage/> : <Navigate to='/'/>}/>
+        <Route path='/problems' element={isSignedIn? <ProblemPage/> : <Navigate to='/'/>}/>
       </Routes>
       <Toaster toastOptions={{duration:3000}}/>
     </>
