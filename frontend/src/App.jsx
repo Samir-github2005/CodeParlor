5bb8c121218b0e94daf1503f2bba839bfcac5a14
Samir Bhakta <bhaktasamir11042005@gmail.com>
2026-03-11 17:14:32 +0530

    added problems page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
-import { ProblemPage } from './pages/ProblemPage.jsx'
+import ProblemsPage from './pages/ProblemsPage.jsx'
 import { useUser } from '@clerk/clerk-react'
 import {Toaster} from 'react-hot-toast'
 import DashboardPage  from './pages/DashboardPage.jsx'
@@ -15,7 +15,7 @@ if(!isLoaded) return null
       <Routes>
         <Route path='/' element={!isSignedIn? <HomePage/> : <Navigate to='/dashboard'/>}/>
         <Route path='/dashboard' element={isSignedIn? <DashboardPage/> : <Navigate to='/'/>}/>
-        <Route path='/problems' element={isSignedIn? <ProblemPage/> : <Navigate to='/'/>}/>
+        <Route path='/problems' element={isSignedIn? <ProblemsPage/> : <Navigate to='/'/>}/>
       </Routes>
       <Toaster toastOptions={{duration:3000}}/>
     </>
